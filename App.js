f5c4979750cd3e862f7e2ab1b70440c9b4b9ff20
yejoonko <yejoon08@ajou.ac.kr>
2020-07-27 15:23:08 +0900

    200727 In Class(Modify App.js)

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button,Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  const{
+    container, textBig,textRed
+  } = styles;
+
+  const clickHandler = () => {
+    console.log("clicked!!");
+  }
+
+  return ( 
+    <View style={container}>
+      <Button 
+          title={"Click me!"} 
+          onPress={clickHandler}
+      />
+      {/* command + /누르면 자동으로 주석되는 듯  */}
+      {/* <Text style = {[textBig, textRed]}>Hello, world!!</Text> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +31,56 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textBig: {
+    fontSize : 32,
+  },
+  textRed: {
+    color : 'red',
+  },
 });
+
+
+// /import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Button, View } from 'react-native';
+
+
+// export default class App extends React.Component() {
+//   state = {
+//     counter: 0,
+//   };
+
+//   render(){
+//     const{
+//       container,
+//   } = styles;
+
+//   const clickHandler = () => {
+//     state.counter++;
+//     console.log("clicked!!");
+//   };
+
+
+//   return (
+//     <View style={container}>
+//     <Button
+//     title={"Click me!"} //버튼
+//     onPress={clickHandler}
+//     //onClick={() => console.log("Clicked!")} //클릭할때마다 console에 띄움
+//     />
+//     {/* <Text style = {[textBig, textRed]}>Hello, world!!</Text> */}
+
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+//  }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
