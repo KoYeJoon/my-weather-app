5fb78db6aeeb15ec8ab8a1e636d261e2995b5c6b
yejoonko <yejoon08@ajou.ac.kr>
2020-07-27 15:33:41 +0900

    Follow 01. Hello, World

--- App.js
+++ App.js
@@ -1,27 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Button,Text, View } from 'react-native';
+import { StyleSheet,Text, View } from 'react-native';
 
-export default function App() {
-  const{
-    container, textBig,textRed
-  } = styles;
-
-  const clickHandler = () => {
-    console.log("clicked!!");
-  }
-
+export default class App extends React.Component {
+  render() {
   return ( 
-    <View style={container}>
-      <Button 
-          title={"Click me!"} 
-          onPress={clickHandler}
-      />
+    <View style={styles.container}>
       {/* command + /누르면 자동으로 주석되는 듯  */}
-      {/* <Text style = {[textBig, textRed]}>Hello, world!!</Text> */}
+      <Text>Hello, world!!</Text> 
       <StatusBar style="auto" />
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
@@ -39,48 +29,3 @@ const styles = StyleSheet.create({
   },
 });
 
-
-// /import { StatusBar } from 'expo-status-bar';
-// import React from 'react';
-// import { StyleSheet, Button, View } from 'react-native';
-
-
-// export default class App extends React.Component() {
-//   state = {
-//     counter: 0,
-//   };
-
-//   render(){
-//     const{
-//       container,
-//   } = styles;
-
-//   const clickHandler = () => {
-//     state.counter++;
-//     console.log("clicked!!");
-//   };
-
-
-//   return (
-//     <View style={container}>
-//     <Button
-//     title={"Click me!"} //버튼
-//     onPress={clickHandler}
-//     //onClick={() => console.log("Clicked!")} //클릭할때마다 console에 띄움
-//     />
-//     {/* <Text style = {[textBig, textRed]}>Hello, world!!</Text> */}
-
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-//  }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
